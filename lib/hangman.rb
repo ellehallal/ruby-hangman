05ea9fa31dd9ed292846a234b2b601774a017886
itsellej <daniellehallal@gmail.com>
2018-11-05 10:42:31 +0000

    Split solution word into an array. Create correct_letters array to length of solution with empty strings as placeholders

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,20 +1,40 @@
 def hangman
-  puts new_word
+  solution = new_word.split("")
+  wrong_guesses = 8
+  correct_letters = [""] * solution.length
+  wrong_letters = []
+
+  puts """
+  Hello, welcome to Hangman!
+  The mystery word contains #{solution.length} letters.
+  You can have up to #{wrong_guesses} incorrect guesses before your man is hanged!
+  Let's begin:
+  """
+  puts "_ " * solution.length
+
 
 
 end
 
 def new_word
-  words = ["apple", "banana", "elephant", "coffee", "dolphin", "window"]
-  words.sample
+  "apple"
+  # words = ["apple", "banana", "elephant", "coffee", "dolphin", "window"]
+  # words.sample
 end
 
+hangman
 
-# User should be greeted by a message
+
+# User should be greeted by a message - done
 # User has a fixed number of guesses
 # Shows which letters have been guessed correctly
 # Shows if a user has won or lost
 
 
 #core method with different methods
-hangman
+
+#stretch goals
+# Show a list of the wrong letters
+# Computer picks a random word - done
+# User can choose to play again
+# draw hangman with each incorrect guess

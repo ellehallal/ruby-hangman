aeea2dd001ac131773ad16c5cdf285c9b286d748
itsellej <daniellehallal@gmail.com>
2018-11-05 10:59:18 +0000

    Push user guess to correct letters array

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,7 +1,7 @@
 def hangman
   solution = new_word.split("")
   wrong_guesses = 8
-  correct_letters = [""] * solution.length
+  correct_letters = ["_"] * solution.length
   wrong_letters = []
 
   puts """
@@ -9,8 +9,28 @@ def hangman
   The mystery word contains #{solution.length} letters.
   You can have up to #{wrong_guesses} incorrect guesses before your man is hanged!
   Let's begin:
+
+  #{correct_letters.join(" ")}
+  Guess a letter:
   """
-  puts "_ " * solution.length
+  guess = gets.chomp
+
+  solution.each_with_index do |letter, index|
+    if guess == letter
+      correct_letters[index] = letter
+      puts """
+      #{letter} is correct!
+      This is what your mystery word looks like so far:
+      #{correct_letters.join(" ")}
+      Please guess another letter:"""
+    # else
+    #   wrong_guesses -=1
+    #   wrong_letters << guess
+    #   puts "That's incorrect. You have #{wrong_guesses} left. Please guess again:"
+    end
+    #while loop
+    #if wrong guesses == 0, game over - game over method
+  end
 
 
 

47f1a53b7f934b996c8c91802bb3e571cb3d654c
itsellej <daniellehallal@gmail.com>
2018-11-05 10:18:58 +0000

    Create hangman method. Create new_word method which returns a word from an array at random

---
+++ lib/hangman.rb
@@ -0,0 +1,18 @@
+def hangman
+
+
+end
+
+def new_word
+  words = %[apple bottle kleenex november Millennium]
+  words[rand(0..words.length)]
+end
+
+
+# User should be greeted by a message
+# User has a fixed number of guesses
+# Shows which letters have been guessed correctly
+# Shows if a user has won or lost
+
+
+#core method with different methods
